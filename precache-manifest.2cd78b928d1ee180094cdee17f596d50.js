8fa1aef1fe8e606b52e6a708ca47d731375f99fd
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-10-07 17:27:07 +0300

    Updates

---
+++ precache-manifest.2cd78b928d1ee180094cdee17f596d50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1615ace0a536c04d16820b03a68bdfb1",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "8151040a652cf34b11f9",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.003135b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.003135b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa84ef0bc2cc176b05c1",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.17b55b4b.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
